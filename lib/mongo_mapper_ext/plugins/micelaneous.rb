2e0601d8ca277fd07a8a1f09ed0f14da40ade772
alexeypetrushin <alexey.petrushin@gmail.com>
2011-04-07 00:31:43 +0400

    add :as_string key converters

--- lib/mongo_mapper_ext/plugins/micelaneous.rb
+++ lib/mongo_mapper_ext/plugins/micelaneous.rb
@@ -15,6 +15,70 @@ module MongoMapper
       end
       
       module ClassMethods
+        STRING_CONVERTORS = {
+          line: {
+            from_string: -> s {(s || "").split(',').collect{|s| s.strip}},
+            to_string:   -> v {v.join(', ')}
+          },
+          yaml: {
+            from_string: -> s {YAML.load s},
+            to_string:   -> v {              
+              # MongoMapper uses it's internal Hash that doesn't support to_yaml
+              hash = {}; v.each{|k, v| hash[k] = v}               
+              hash.to_yaml.strip
+            }
+          },
+          json: {
+            from_string: -> s {JSON.parse s},
+            to_string:   -> v {          
+              # MongoMapper uses it's internal Hash that doesn't support to_yaml
+              hash = {}; v.each{|k, v| hash[k] = v}               
+              hash.to_json.strip
+            }
+          }
+        }
+        
+        # 
+        # custom key accessors
+        # 
+        def key(*args)
+          key = super                    
+          if converter_name = key.options[:as_string]
+            key_name = key.name.to_sym            
+            available_as_string key_name, converter_name
+            attr_protected "#{key_name}_as_string".to_sym if key.options[:protected]            
+          end
+          key
+        end
+        
+        def available_as_string key_name, converter_name
+          converter = STRING_CONVERTORS[converter_name]
+          raise "unknown converter name :#{converter_name} for :#{key_name} key!" unless converter
+          
+          from_string, to_string = converter[:from_string], converter[:to_string]
+          key_name_as_string = "#{key_name}_as_string".to_sym
+          define_method key_name_as_string do
+            cache[key_name_as_string] ||= to_string.call(self.send(key_name))
+          end
+          define_method "#{key_name_as_string}=" do |value|
+            cache.delete key_name_as_string                        
+            self.send "#{key_name}=", from_string.call(value)
+          end
+        end
+        
+        def available_as_yaml key_name
+          raise "delimiter not specified for :#{key_name} key!" unless delimiter
+          method = "#{key_name}_as_string"
+          define_method method do
+            self.send(key_name).join(delimiter)
+          end
+          define_method "#{method}=" do |value|            
+            value = (value || "").split(delimiter.strip).collect{|s| s.strip}
+            self.send "#{key_name}=", value
+          end
+        end
+        
+        
         # 
         # model_name
         # 
--- spec/micelaneous_spec.rb
+++ spec/micelaneous_spec.rb
@@ -9,10 +9,14 @@ describe "MongoMapper micelaneous" do
     MongoMapper.database = 'test'
   end
   
+  after :all do
+    remove_constants %w(UpsertSample AsStringSample)
+  end  
+  
   before do
     @db.collection('test').drop
     @coll = @db.collection('test')
-  end
+  end    
     
   it "upsert should update" do
     id = @coll.save count: 2
@@ -48,6 +52,72 @@ describe "MongoMapper micelaneous" do
       @model.upsert :$inc => {counter: 1}
       @model.reload
       @model.counter.should == 2
+    end
+  end
+  
+  describe "as_string" do
+    before do
+      @convertors = MongoMapper::Plugins::Micelaneous::ClassMethods::STRING_CONVERTORS
+      @convertors[:test_convertor] = {
+        from_string: -> s {"from_string: #{s}"},
+        to_string:   -> v {"to_string: #{v}"}
+      }
+    end
+    
+    it ":line convertor" do      
+      v = ['a', 'b']
+      str_v = 'a, b'
+      @convertors[:line][:from_string].call(str_v).should == v
+      @convertors[:line][:to_string].call(v).should == str_v
+    end
+    
+    it ":yaml convertor" do
+      v = {'a' => 'b'}
+      str_v = v.to_yaml.strip
+      
+      @convertors[:yaml][:from_string].call(str_v).should == v
+      @convertors[:yaml][:to_string].call(v).should == str_v
+    end
+    
+    it ":json convertor" do
+      v = {'a' => 'b'}
+      str_v = v.to_json.strip
+      @convertors[:json][:from_string].call(str_v).should == v
+      @convertors[:json][:to_string].call(v).should == str_v
+    end
+    
+    it ":key extension" do
+      class ::AsStringSample
+        include MongoMapper::Document
+        plugin MongoMapper::Plugins::Micelaneous
+        
+        key :key, String, as_string: :test_convertor
+        key :protected_key, String, as_string: :test_convertor, protected: true
+      end
+    
+      o = AsStringSample.new
+      
+      # get
+      o.key_as_string.should == 'to_string: '
+      o.key = 'value'
+      o.clear_cache
+      o.key_as_string.should == 'to_string: value'
+      
+      # set
+      o.key_as_string = ''
+      o.key.should == 'from_string: '
+      o.key_as_string = 'value'
+      o.key.should == 'from_string: value'
+      
+      # mass assignment
+      o.key = ''
+      o.update_attributes key_as_string: 'value'
+      o.key.should == 'from_string: value'
+      
+      # protection
+      o.protected_key = ''
+      o.update_attributes protected_key_as_string: 'value'
+      o.protected_key.should == ''
     end
   end
 end
