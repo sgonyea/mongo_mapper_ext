6fee134e30e0728ad2264adfbcfed5a0e11c03fb
alexeypetrushin <alexey.petrushin@gmail.com>
2011-05-20 03:20:52 +0400

    small refactoring with lots of changes

--- lib/mongo_mapper_ext/rad.rb
+++ lib/mongo_mapper_ext/rad.rb
@@ -3,4 +3,7 @@ require 'rad'
 require 'mongo_mapper_ext/mongo_mapper'
 require 'mongo_mapper_ext/carrier_wave'
 
+module Models
+end
+
 require 'mongo_mapper_ext/rad/file_uploader'
--- lib/mongo_mapper_ext/rad/file_uploader.rb
+++ lib/mongo_mapper_ext/rad/file_uploader.rb
@@ -1,28 +1,26 @@
 require 'carrierwave/processing/mini_magick'
 
-module Models
-  class FileUploader < CarrierWave::Uploader::Base
-    include CarrierWave::MiniMagick
-    
-    storage rad.config.fs.type(:file)
+class Models::FileUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
   
-    # def sanitize_regexp
-    #   /[^[:word:]\.\-\+\s_]/i
-    # end
-  
-    def file_path
-      "#{rad.config.fs.prefix('/fs')}/system/#{model.class.model_name.underscore}/#{model.id}"
-    end
-  
-    def store_dir
-      "#{root}#{file_path}"
-    end
-  
-    def extension_white_list
-      [/.*/]
-    end
-  
-    def cache_dir; rad.config.fs.cache_path! end  
-    def root; rad.config.fs.path! end  
+  storage rad.config.fs.type(:file)
+
+  # def sanitize_regexp
+  #   /[^[:word:]\.\-\+\s_]/i
+  # end
+
+  def file_path
+    "#{rad.config.fs.prefix('/fs')}/system/#{model.class.model_name.underscore}/#{model.id}"
   end
+
+  def store_dir
+    "#{root}#{file_path}"
+  end
+
+  def extension_white_list
+    [/.*/]
+  end
+
+  def cache_dir; rad.config.fs.cache_path! end  
+  def root; rad.config.fs.path! end  
 end
