95bb7127df3941ca015f56dc83d47676cb2283b6
alexeypetrushin <alexey.petrushin@gmail.com>
2011-05-22 13:59:32 +0400

    small updates

--- spec/carrier_wave/carrier_wave_spec.rb
+++ spec/carrier_wave/carrier_wave_spec.rb
@@ -4,58 +4,108 @@ describe "MongoMapper & CarrierWave" do
   with_mongo_mapper
   with_tmp_spec_dir
   
-  before :all do
-    class PlaneUploader < CarrierWave::Uploader::Base
-      require 'carrierwave/processing/mini_magick'
+  describe "file_key" do
+    before :all do
+      class PlaneUploader < CarrierWave::Uploader::Base
+        require 'carrierwave/processing/mini_magick'
 
-      include CarrierWave::MiniMagick      
+        include CarrierWave::MiniMagick      
       
-      storage :file
+        storage :file
       
-      version :icon do
-        process convert: :png
-        process resize_to_fit: [50, 50]
+        version :icon do
+          process convert: :png
+          process resize_to_fit: [50, 50]
+        end
+      
+        class << self
+          attr_accessor :store_dir
+        end      
+      
+        def store_dir
+          PlaneUploader.store_dir
+        end
+      
+        def root
+          PlaneUploader.store_dir
+        end
       end
-      
-      class << self
-        attr_accessor :store_dir
-      end      
-      
-      def store_dir
-        PlaneUploader.store_dir
-      end
-      
-      def root
-        PlaneUploader.store_dir
-      end
+    
+      class Plane
+        include MongoMapper::Document
+  
+        file_key :image, PlaneUploader
+      end        
     end
+    after(:all){remove_constants :Plane, :PlaneUploader}
+  
+    before do
+      PlaneUploader.store_dir = "#{spec_dir}/data"
+      @file = File.new "#{spec_dir}/plane.jpg"
+    end
+    after{@file.close}
+  
+    it "basic" do
+      plane = Plane.create! image: @file
+      plane.image.current_path.should =~ /\/plane.jpg/
+      File.should exist("#{spec_dir}/data/plane.jpg")
+    end
+  
+    it "path format" do
+      plane = Plane.create! image: @file
+      plane.image.url.should == '/plane.jpg'
+      plane.image.icon.url.should =~ /\/plane\.icon\.jpg/
     
-    class Plane
-      include MongoMapper::Document
-  
-      file_key :image, PlaneUploader
-    end        
-  end
-  after(:all){remove_constants :Plane, :PlaneUploader}
-  
-  before do
-    PlaneUploader.store_dir = "#{spec_dir}/data"
-    @file = File.new "#{spec_dir}/plane.jpg"
-  end
-  after{@file.close}
-  
-  it "basic" do
-    plane = Plane.create! image: @file
-    plane.image.current_path.should =~ /\/plane.jpg/
-    File.should exist("#{spec_dir}/data/plane.jpg")
+      plane.image.icon.current_path.should =~ /\/plane\.icon\.jpg/
+      File.should exist("#{spec_dir}/data/plane.icon.jpg")
+    end
   end
   
-  it "path format" do
-    plane = Plane.create! image: @file
-    plane.image.url.should == '/plane.jpg'
-    plane.image.icon.url.should =~ /\/plane\.icon\.jpg/
-    
-    plane.image.icon.current_path.should =~ /\/plane\.icon\.jpg/
-    File.should exist("#{spec_dir}/data/plane.icon.jpg")
-  end
+  # describe "file_keys" do
+  #   before :all do
+  #     # class PlaneUploader < CarrierWave::Uploader::Base      
+  #     #   storage :file
+  #     # 
+  #     #   class << self
+  #     #     attr_accessor :store_dir
+  #     #   end      
+  #     # 
+  #     #   def store_dir
+  #     #     PlaneUploader.store_dir
+  #     #   end
+  #     # 
+  #     #   def root
+  #     #     PlaneUploader.store_dir
+  #     #   end
+  #     # end
+  #   
+  #     class Plane
+  #       include MongoMapper::Document
+  # 
+  #       # file_keys :images, PlaneUploader
+  #     end        
+  #   end
+  #   after(:all){remove_constants :Plane, :PlaneUploader}
+  # 
+  #   before do
+  #     PlaneUploader.store_dir = "#{spec_dir}/data"
+  #     @file = File.new "#{spec_dir}/plane.jpg"
+  #   end
+  #   after{@file.close}
+  # 
+  #   it "basic" do
+  #     plane = Plane.create! image: @file
+  #     plane.image.current_path.should =~ /\/plane.jpg/
+  #     File.should exist("#{spec_dir}/data/plane.jpg")
+  #   end
+  # 
+  #   it "path format" do
+  #     plane = Plane.create! image: @file
+  #     plane.image.url.should == '/plane.jpg'
+  #     plane.image.icon.url.should =~ /\/plane\.icon\.jpg/
+  #   
+  #     plane.image.icon.current_path.should =~ /\/plane\.icon\.jpg/
+  #     File.should exist("#{spec_dir}/data/plane.icon.jpg")
+  #   end
+  # end
 end
