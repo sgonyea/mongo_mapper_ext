5cb89604c0bc2f4f86368059eaf7399b512eb090
alexeypetrushin <alexey.petrushin@gmail.com>
2011-05-22 19:59:12 +0400

    tmp

--- spec/mongo_mapper/mongo_mapper_spec.rb
+++ spec/mongo_mapper/mongo_mapper_spec.rb
@@ -6,31 +6,40 @@ describe "Special test to check some errors in the MongoMapper" do
   end
   
   after :all do
-    Object.send :remove_const, :Plane, :PlaneImage if Object.const_defined? :AnObject
+    Object.send :remove_const, :ThePost, :TheComment if Object.const_defined? :AnObject
   end
       
   it "strange behaviour of EmbeddedDocument" do
-    class PlaneImage
+    class TheComment
       include MongoMapper::EmbeddedDocument
       
-      key :data, String      
-      alias_method :original_data=, :data=
-      def data= value
-        $id_before_save = object_id
-        self.original_data = value
-      end      
+      key :text, String
+      # alias_method :original_text=, :text=
+      # def text= text
+      #   $id_before_save = object_id
+      #   self.original_text = text
+      # end      
       
-      after_save :check_my_id
-      def check_my_id
-        $id_after_save = object_id
+      before_save do
+        p self
       end
+      
+      after_save do
+        p self
+      end
+      
+      # after_save :check_my_id
+      # def check_my_id
+      #   $id_after_save = object_id
+      # end
     end
-    class Plane
+    
+    class ThePost
       include MongoMapper::Document
       has_many :images, class_name: 'PlaneImage'
     end    
     
     Plane.create! images: [PlaneImage.new(data: 'some image')]
-    $id_before_save.should == $id_after_save
+    # $id_before_save.should == $id_after_save
   end
 end
